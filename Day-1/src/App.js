5bec1a8fc4d1f15dd2fcfacbf8083aa92887cb86
krithik10 <153708683+krithik10@users.noreply.github.com>
2023-12-13 12:12:49 +0530

    Add files via upload

---
+++ Day-1/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+// import Main from "./components/Main";
+// import About from "./components/About";
+import Login from "./components/Login";
+
+function App() {
+  return (
+   
+      
+        <div className="App">
+      <NavBar/>
+      {/* <Main/> */}
+      <Login/>
+      
+      </div>
+    
+  );
+}
+
+export default App;
